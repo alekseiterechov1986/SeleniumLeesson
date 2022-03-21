7dd61754459dee8f4cf29cd36a7b2f5f9df7af61
alekseiterechov1986 <alekseiterechov1986@mail.ru>
2022-03-21 23:08:42 +0300

    vlogenie

--- src/test/java/second.java
+++ src/test/java/second.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class second {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class second {
+    public class SecondTest {
+
+        private WebDriver driver;
+
+        @BeforeClass
+        public void setUp(){
+            System.setProperty("webdriver.chrome.driver", "D:\\MavenGitTestNG\\src\\main\\resources\\chromedriver.exe");
+            driver = new ChromeDriver();
+
+        }
+
+        @AfterClass
+        public void finish(){
+            driver.quit();
+
+        }
+
+        @Test
+        public void openGoogle(){
+            driver.get("https://www.google.by");
+        }
+
+
+    }
 }
